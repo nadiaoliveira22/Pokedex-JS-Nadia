90807b9f8bc154d63af1b962ecf838b4b6e62e4a
Nádia Oliveira <rtv.nadia@gmail.com>
2026-03-12 21:50:18 -0300

    funcionando ?

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Funcionou")
+const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+const pokemonlist = document.getElementById("lista-pokemon")
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <p class="nome">${pokemon.name}</p>
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="Bulbasaur">
+        </li>
+                
+    `;
+}
+
+function getPokemonDetails(pokemon){
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonresponse) => jsonresponse.results)
+    .then((list) => list.map(getPokemonDetails))
+    .then((details) => Promise.all(details))
+    .then((newList) => pokemonlist.innerHTML = newList.map(convertPokemonToLi).join(""))
+    .catch((error) => console.log(error));
