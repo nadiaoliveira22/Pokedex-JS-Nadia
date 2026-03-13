5abb1d667c46a7d5c2db3f4fe9b6d1ae6d7da5cb
Nádia Oliveira <rtv.nadia@gmail.com>
2026-03-13 20:16:33 -0300

    js atualizado com await async

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Pokedex Javascript
+
 Estudo para desenvolver com HTML, CSS e JavaScript uma Pokedex
-Este é meu primeiro projeto front-end, mostrando que consegui entender o uso de uma API nele
+Este é meu primeiro projeto front-end, mostrando que consegui entender o uso de uma API nele.
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
 const pokemonlist = document.getElementById("lista-pokemon")
 
 function convertPokemonToLi(pokemon){
+    console.log(pokemon.types[0].type.name)
     return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.types[0].type.name}">
             <p class="nome">${pokemon.name}</p>
             <img src="${pokemon.sprites.other.dream_world.front_default}" alt="Bulbasaur">
         </li>
@@ -11,9 +12,9 @@ function convertPokemonToLi(pokemon){
     `;
 }
 
-function getPokemonDetails(pokemon){
-    return fetch(pokemon.url)
-    .then((response) => response.json())
+async function getPokemonDetails(pokemon){
+    return await fetch(pokemon.url)
+    .then(async(response) => await response.json())
 }
 
 fetch(url)
